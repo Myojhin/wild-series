71303fa6a069ee0a4fb0f3d341ce361cf704d2fb
Alix <alix.champagnol@gmail.com>
2024-09-05 11:32:39 +0200

    ajout category et program dans schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,21 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+-- Création de la table category
+CREATE TABLE IF NOT EXISTS category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+-- Création de la table program
+CREATE TABLE IF NOT EXISTS program (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    synopsis TEXT NOT NULL,
+    poster VARCHAR(255),
+    country VARCHAR(255),
+    year INT,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
